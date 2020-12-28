aa1cf749a4cc0cba63be8eb83ba319e7d5afd358
viktoria-shp <vvitashp@gmail.com>
2020-12-28 18:11:34 +0200

    change

--- src/components/sections/TheWorks.vue
+++ src/components/sections/TheWorks.vue
@@ -73,9 +73,6 @@ export default {
       width: 71%;
       margin-bottom: 48px;
     }
-    &__slide {
-      @include padding-hack;
-    }
     &__img {
       width: 100%;
       margin-bottom: 40px;
